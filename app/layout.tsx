ac4a79c3472a7426bba97ee17e24c80dd8d020b5
Thiago <thiagodelgado.dev@gmail.com>
2024-10-05 19:25:24 -0300

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter as FontSans } from "next/font/google"
 
 import './globals.css'
 import { Metadata } from "next"
+import { cn } from "@/lib/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
